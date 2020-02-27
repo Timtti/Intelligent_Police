8fc8a786253ccaf1eb12abeed3fccac6e5518592
Nina Z <ninazrdl@gmail.com>
2020-02-27 20:26:28 +0000

    Test Police Agent
    Should move and steer.

--- Police-Unity/Assets/Scripts/PoliceAgent.cs
+++ Police-Unity/Assets/Scripts/PoliceAgent.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.Sprites;
 using MLAgents;
+using MLAgents.Sensor;
 
 public class PoliceAgent : Agent
 {
@@ -12,20 +12,14 @@ public class PoliceAgent : Agent
     Rigidbody2D rbody;
     Vector2 initPos;
     Quaternion initRota;
-    RayPerception2D rayper;
-
-    public new Camera camera;
-    private Texture2D targetTexture;
-
+    VectorSensor sensor = new VectorSensor(4);
+    float power = 5f;
 
     public override void InitializeAgent()
     {
         this.rbody = GetComponent<Rigidbody2D>();
-        this.camera = GetComponent<Camera>();
-
         this.initPos = this.transform.position;
         this.initRota = this.transform.rotation;
-        this.target = GameObject.Find("Target");
     }
     public override void AgentReset()
     {
@@ -38,38 +32,65 @@ public class PoliceAgent : Agent
         this.crush = true;
         if (collision.gameObject.tag == "Target")
         {
-            AddReward(10.0f);
+            AddReward(100f);
+        }
+        else
+        {
+            AddReward(-10f);
         }
     }
     public override void AgentAction(float[] vectorAction)
     {
-        Vector2 handling = new Vector2(Random.Range(-100f, 100f), Random.Range(-100f, 100f));
-        //rbody.AddForce(handling);
-        float handle_x = Random.Range(-1f, 1f);
-        float handle_y = Random.Range(-1f, 1f);
-        this.gameObject.transform.Rotate(new Vector3(handle_x, handle_y));
-
-        this.rbody.velocity = this.gameObject.transform.rotation * new Vector2(0, 20);
+        System.Random rnd = new System.Random();
+        int hand = rnd.Next(0,3);
+        transform.Rotate(Handling(hand));
         
-        float distance = Vector2.Distance(target.transform.positoin,this.transform.position);
-        if (distance < 5f) {
-            AddReward(3.0f)
-        }
+        Vector3 velocity = transform.rotation * Pedalwork();
+        transform.position -= velocity * Time.deltaTime;
+
+        AddReward(0.1f);
         if (this.crush)
         {
             Done();
         }
     }
+    public Vector3 Handling(int choice=0)
+    {
+        if (choice == 1)
+        {
+            return Vector3.forward;
+        }
+        if (choice == 2)
+        {
+            return Vector3.back;
+        }
+        else
+        {
+            return new Vector3(0,0,0);
+        }
+    }
+    
+    public Vector3 Pedalwork(int min = 0, int max = 0)
+    {
+        if(min == 0 & max == 0){
+            return new Vector3(power, 0, 0);
+        }
+        else
+        {
+            System.Random randf = new System.Random();
+            float f = (float)randf.Next(min,max);
+            return new Vector3(f,0,0);
+        }
+    }
+
     public override void CollectObservations()
     {
-        CollectVideo();
-        var cols = targetTexture.GetPixels();
-        foreach (var col in cols)
-        {
-            AddVectorObs(col.b);
-        }
+        // Target and Agent positions
+        sensor.AddObservation(this.transform.position);
 
-        AddVectorObs(this.transform.position);
+        // Agent velocity
+        sensor.AddObservation(rbody.velocity.x);
+        sensor.AddObservation(rbody.velocity.y);
     }
     public override float[] Heuristic()
     {
@@ -77,14 +98,5 @@ public class PoliceAgent : Agent
         action[0] = Input.GetAxis("Horizontal");
         action[1] = Input.GetAxis("Vertical");
         return action;
-    }
-
-    void CollectVideo()
-    {
-        var tex = camera.targetTexture;
-        targetTexture = new Texture2D(tex.width, tex.height, TextureFormat.ARGB32, false);
-        RenderTexture.active = camera.targetTexture;
-        targetTexture.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
-        targetTexture.Apply();
     }
 }
